dd07a1bc460a2b6999d7b16847fc08662de245c8
shabil15 <mohammedshabil15@gmail.com>
2023-12-19 09:49:11 +0530

    Fibonacci done

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+function fibonacci(n) {
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+console.log(fibonacci(2));
+console.log(fibonacci(5));
+console.log(fibonacci(7));
+
+
+
+function factorial (n) {
+  let result = 1
+
+  for(let i=2 ;i<= n ;i++ ){
+    result = result*i
+  }
+  return result
+}
