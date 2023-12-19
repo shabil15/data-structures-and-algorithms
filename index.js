6cef2bcb166c60d5a259ab33098365937239807c
shabil15 <mohammedshabil15@gmail.com>
2023-12-19 09:54:48 +0530

    is Prime function

--- index.js
+++ index.js
@@ -20,7 +20,23 @@ function factorial (n) {
   for(let i=2 ;i<= n ;i++ ){
     result = result*i
   }
-  return result
+  return result 
 }
 
-console.log(factorial(5));
+// console.log(factorial(5));
+
+function isPrime(n){
+  if(n<2){
+    return false
+  }
+  for (let i=2;i< n ;i ++)  {
+      if(n%i ==0  ){
+        return false
+      }
+  }
+  return true
+}
+
+console.log(isPrime(3));
+console.log(isPrime(5));
+console.log(isPrime(6));  
