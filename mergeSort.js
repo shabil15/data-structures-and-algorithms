0fe71049aa3a80a2baf63bb5f2f402bb259b48ea
shabil15 <mohammedshabil15@gmail.com>
2023-12-25 18:59:00 +0530

    merge sort done

--- mergeSort.js
+++ mergeSort.js
@@ -4,20 +4,22 @@ function MergeSort(arr){
   }
   let mid = Math.floor(arr.length/2)
   let leftArr = arr.slice(0,mid)
-  let rightArr = arr.slice(mid)
+  let rightArr =arr.slice(mid)
   return Merge(MergeSort(leftArr),MergeSort(rightArr))
 }
 
 function Merge(leftArr,rightArr){
-  let sortedArr = []
+  let sorted=[]
   while(leftArr.length && rightArr.length){
     if(leftArr[0]<rightArr[0]){
-      sortedArr.push(leftArr.shift())
+      sorted.push(leftArr.shift())
     }else{
-      sortedArr.push(rightArr.shift())
+      sorted.push(rightArr.shift())
     }
+
   }
-  return [...sortedArr,...leftArr,...rightArr]
+  return [...sorted,...leftArr,...rightArr]
 }
-const arr = [-2,45,5,66,457,34,-4]
+
+const arr=[-5,354,34,45,7,9,-2,5]
 console.log(MergeSort(arr));
